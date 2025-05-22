b66afad0fb77737b6ec52637436cbfc75065e985
Ning <lance930612@gmail.com>
2025-05-23 00:48:48 +0800

    Initial Commit

---
+++ Trace/TraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceApp.swift
+//  Trace
+//
+//  Created by 王政甯 on 2025/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TraceTests/TraceTests.swift
@@ -0,0 +1,17 @@
+//
+//  TraceTests.swift
+//  TraceTests
+//
+//  Created by 王政甯 on 2025/5/23.
+//
+
+import Testing
+@testable import Trace
+
+struct TraceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
